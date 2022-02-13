4f8a7055bb0d80e2f8244c060eb16c296cd1794d
Igor Pedrosa <ilap1036@gmail.com>
2022-02-13 12:27:46 -0300

    melhorias e tratamento de erro login

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const readline = require('readline-sync');
 
 console.log('📽 ✍️ 🤖 Letterboxd Reviews in JSON');
 (async () => {
+    let pages = await readline.question('Quantidade de páginas, em seu histórico de reviews, desejadas OU pressione "Enter" para trazer todas: ', {
+        encoding: 'utf-8'
+    });
     const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
     await page.goto('https://letterboxd.com', {
@@ -13,14 +16,18 @@ console.log('📽 ✍️ 🤖 Letterboxd Reviews in JSON');
         timeout: 0
     });
 
-    let pages = readline.question('Quantidade de páginas, em seu histórico de reviews, desejadas OU pressione "Enter" para trazer todas: ', {
-        encoding: 'utf-8'
-    });
-
     await page.click('[href="/sign-in/"]');
     await page.type('input[type="email"]', process.env.EMAIL );
     await page.type('input[type="password"]', process.env.PASS);
     await page.click('input[type="submit"]');
+    await delay(3000);
+    const error = await page.evaluate(()=>{
+        return document.querySelector('div.errormessage').innerText;
+    })
+    
+    if(error !== ''){
+        throw new Error('Login falhou. Confira o seu arquivo .env');
+    }
     await page.waitForSelector('.nav-account');
     const userName = await page.evaluate(() => document.querySelector('.nav-account a').innerHTML.split('>').pop());
     await page.goto(`https://letterboxd.com/${userName}/films/reviews/`, {
@@ -38,7 +45,7 @@ console.log('📽 ✍️ 🤖 Letterboxd Reviews in JSON');
     let allReviews = [];
 
     for(let i = 1; i<=totalReviewPages; i++){
-        const clicks = await page.evaluate(()=> document.querySelectorAll('a.reveal.js-reveal'))
+        const clicks = await page.evaluate(()=> document.querySelectorAll('a.reveal.js-reveal'));
 
         for(let i = 0; i<Object.keys(clicks).length; i++ ){
             await page.click('a.reveal.js-reveal');
